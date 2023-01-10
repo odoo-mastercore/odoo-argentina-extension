b7f2929106cbf03c92e3a977d38814d33617ec41
Morena <morenasandroni.b@gmail.com>
2023-01-10 18:07:05 -0300

    [WIP] Support Ticket #15110

---
+++ l10n_ar_afip_activity/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2023-Present.
+# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+{
+    "name": "l10n_ar_afip_activity",
+    "summary": "",
+    'version': "15.0",
+    'author': 'SINAPSYS GLOBAL SA || MASTERCORE SAS',
+    'website': "http://sinapsys.global",
+    'category': "contact",
+    "license": "AGPL-3",
+    "depends": [],
+    "data": [
+        'data/afip_activity_data.xml',
+    ],
+}

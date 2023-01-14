f9d71576f79aab233ac00d067fd9608461090fc2
odoo-mastercore <github@sinapsys.global>
2023-01-14 18:29:17 -0300

    [MIG][ADD] l10n_ar_afip_activity

---
+++ l10n_ar_afip_activity/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2023-Present.
+# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+{
+    "name": "l10n_ar_afip_activity",
+    "summary": "",
+    'version': "16.0.1",
+    'author': 'SINAPSYS GLOBAL SA || MASTERCORE SAS',
+    'website': "http://sinapsys.global",
+    'category': "contact",
+    "license": "AGPL-3",
+    "depends": ['l10n_ar_ux'],
+    "data": [
+        'data/afip_activity_data.xml',
+    ],
+}

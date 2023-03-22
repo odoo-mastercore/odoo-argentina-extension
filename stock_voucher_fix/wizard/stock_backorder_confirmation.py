48398b069382eb98c7627fa3cfad60cd3624543a
Elvis Paez <elvispaez18@gmail.com>
2023-03-22 16:22:28 -0400

    [ADD] stock voucher fix

--- stock_voucher_fix/wizard/stock_backorder_confirmation.py
+++ stock_voucher_fix/wizard/stock_backorder_confirmation.py
@@ -10,7 +10,7 @@ class StockBackorderConfirmation(models.TransientModel):
     _inherit = 'stock.backorder.confirmation'
 
     def process(self):
-        super().process()
+        # super().process()
         pickings = self.env['stock.picking'].browse(
             # if we came, for eg, from a sale order, active_id would be the
             # sale order id
@@ -27,3 +27,19 @@ class StockBackorderConfirmation(models.TransientModel):
             }
 
 
+    def process_cancel_backorder(self):
+        super().process_cancel_backorder()
+        pickings = self.env['stock.picking'].browse(
+            # if we came, for eg, from a sale order, active_id would be the
+            # sale order id
+            # self._context.get('active_id'))
+            # TODO we should also fix odoo methods
+            self._context.get('picking_ids')).filtered('book_required')
+        if pickings:
+            return {
+                'actions': [
+                    {'type': 'ir.actions.act_window_close'},
+                    pickings.do_print_voucher(),
+                ],
+                'type': 'ir.actions.act_multi',
+            }

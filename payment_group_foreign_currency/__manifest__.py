79e42789a66b5d6b78e53f80b8d62de25bb55153
Reydi Hernandez <rhe@sinapsys.global>
2023-03-28 10:20:17 -0400

    [FIX]Support Ticket #2303-016459

---
+++ payment_group_foreign_currency/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2023-Present.
+# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+{
+    "name": "Payment Group in Foreign currency",
+    "summary": "",
+    'version': "16.0.0.1",
+    'author': 'SINAPSYS GLOBAL SA || MASTERCORE SAS',
+    'website': "http://sinapsys.global",
+    'category': "contact",
+    "license": "AGPL-3",
+    "depends": ['account_payment_group'],
+    "data": [
+        "views/account_payment_group_view.xml",
+    ],
+}
---
+++ payment_group_foreign_currency/models/account_payment.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2022-Present.
+#
+#
+###############################################################################
+from odoo import models, fields, api, _
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
+
+    @api.onchange('journal_id')
+    def _onchange_compute_amount_currency(self):
+        for rec in self:
+            pass
+            if rec.other_currency and rec.payment_group_id:
+                if rec.payment_group_id.payments_amount <= 0:
+                    rec.amount = rec.payment_group_id.selected_finacial_debt
+                if rec.payment_group_id and rec.payment_group_id.payments_amount > 0:
+                    rec.amount = 0
+                    payments_amount = rec.payment_group_id.selected_finacial_debt - \
+                        rec.payment_group_id.payments_amount
+                    rec.amount = rec.company_id.currency_id._convert(
+                        payments_amount, rec.currency_id, rec.company_id, rec.date)
+            if not rec.other_currency and rec.payment_group_id:
+                rec.amount = rec.payment_group_id.selected_finacial_debt
+                if rec.payment_group_id and rec.payment_group_id.payments_amount > 0:
+                    payments_amount = rec.payment_group_id.payments_amount - rec.amount
+                    rec.amount = rec.payment_group_id.selected_finacial_debt - \
+                        payments_amount
+
+    @api.onchange('date')
+    def _onchange_compute_amount_currency_date(self):
+        for rec in self:
+            if rec.other_currency and rec.payment_group_id:
+                rec.amount_company_currency = rec.currency_id._convert(
+                    rec.amount, rec.company_id.currency_id,
+                    rec.company_id, rec.date)

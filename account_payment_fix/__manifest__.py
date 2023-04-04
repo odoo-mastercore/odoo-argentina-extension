e5a081f4c87dd913bb6b35243f09442b99556807
José Bracho <brachojgbp@gmail.com>
2023-04-04 09:53:39 -0400

    [ADD] account_payment_fix added

---
+++ account_payment_fix/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2023-Present.
+# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+{
+    "name": "Account Payment Fix",
+    "summary": "",
+    'version': "16.0.1",
+    'author': 'SINAPSYS GLOBAL SA || MASTERCORE SAS',
+    'website': "http://sinapsys.global",
+    'category': "sale",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        "account_withholding"
+    ],
+    "data": [],
+}
---
+++ account_payment_fix/models/account.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2023-Present.
+# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+from odoo import fields, models, _, api
+from odoo.exceptions import UserError, ValidationError
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    def action_draft(self):
+        # account
+        self.move_id.button_draft()
+
+        # hr_expense
+        if self.reconciled_bill_ids.expense_sheet_id:
+            self.reconciled_bill_ids.expense_sheet_id.write({'state': 'post'})
+
+        # Adhoc account-payment with improvements
+        withholdings = self.filtered(lambda x: x.tax_withholding_id)
+        for withholding in withholdings:
+            liquidity_lines, counterpart_lines, writeoff_lines = withholding._seek_for_lines()
+            liquidity_lines._write({
+                'tax_repartition_line_id': None,
+                'tax_line_id': None,
+            })

2a1d2fd2fb757aeb6b95a82178da0f0eb42072b9
Elvis Paez <elvispaez18@gmail.com>
2023-05-10 17:32:25 -0400

    [ADD] 16725

--- l10n_ar_remito_sale_order/__manifest__.py
+++ l10n_ar_remito_sale_order/__manifest__.py
@@ -9,7 +9,7 @@
 {
     "name": "l10n ar remito sale order",
     "summary": "",
-    'version': "16.0.0.0.1",
+    'version': "16.0.0.0.3",
     'author': 'SINAPSYS GLOBAL SA || MASTERCORE SAS',
     'website': "http://sinapsys.global",
     'category': "sale",

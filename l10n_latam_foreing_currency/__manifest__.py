a9fd7027e1f0aca424ead3b77886cf7b44dae9e1
Morena <morenasandroni.b@gmail.com>
2023-05-12 15:52:38 -0300

    [ADD] foreign currency

---
+++ l10n_latam_foreing_currency/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2021-Present.
+# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+{
+    'name': 'Foreign Currency',
+    'version': '15.0.9.2',
+    'description': '',
+    'summary': '',
+    'author': "SINAPSYS GLOBAL SA || MASTERCORE SAS",
+    'website': "http://sinapsys.global",
+    'license': 'AGPL-3',
+    'category': 'Localization',
+    'depends': [
+        'account',
+        'base',
+        'l10n_ar',
+    ],
+    'data': [
+        'views/account_move_view.xml',
+        'views/res_company_view.xml'
+    ],
+    'auto_install': False,
+    'application': False,
+}
---
+++ l10n_latam_foreing_currency/models/account_move.py
@@ -0,0 +1,44 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+from odoo.tools import float_compare, frozendict
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    company_foreign_currency_id = fields.Many2one(string='Foreign Company Currency', readonly=True,
+        related='company_id.foreign_currency_id')
+    amount_total_signed_foreign = fields.Monetary(string='Total Signed', readonly=True, store=True, currency_field='company_foreign_currency_id', compute='_compute_foreigns')
+    amount_residual_foreign = fields.Monetary(string='Amount Due', store=True, currency_field='company_foreign_currency_id', compute='_compute_foreigns')
+    
+    @api.depends('amount_total_signed', 'amount_residual')
+    def _compute_foreigns(self):
+        for rec in self:
+            if rec.move_type == 'out_invoice' and not rec.debit_origin_id:
+                _logger.warning('if rec.move_type')
+                if rec.currency_id == rec.company_foreign_currency_id:
+                    _logger.warning('ec.currency_id')
+                    rec.amount_total_signed_foreign = rec.amount_total_in_currency_signed
+                    rec.amount_residual_foreign = rec.amount_residual
+                else:
+                    _logger.warning('ec.else')
+                    
+                    if rec.l10n_ar_currency_rate != 0.00:
+                        _logger.warning('ec.kskkss')
+                        rate = 1/rec.l10n_ar_currency_rate
+                    else:
+                        _logger.warning('ec.jskjsjkskelse')
+                        rate = rec.env['res.currency.rate'].search([
+                            ('currency_id','=', rec.company_foreign_currency_id.id),
+                            ('name', '<=', rec.invoice_date)], limit=1).inverse_company_rate
+
+                    rec.amount_total_signed_foreign = rec.amount_total_in_currency_signed / rate
+                    rec.amount_residual_foreign = rec.amount_residual / rate
+            
+            else:    
+                _logger.warning('nota de debito')
+                rec.amount_total_signed_foreign = 0
+                rec.amount_residual_foreign = 0
+                

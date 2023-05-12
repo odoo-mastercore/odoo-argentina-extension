8a25ed094d83ef653ce213a9ef1ab435a8e6dad7
Denidson Alvarez <dalvarez@code-war.com>
2023-05-12 18:52:11 -0400

    [IMP] report foreign currency
    Add amount_untaxed_signed_foreign

--- l10n_latam_foreing_currency/models/account_move.py
+++ l10n_latam_foreing_currency/models/account_move.py
@@ -12,8 +12,9 @@ class AccountMove(models.Model):
         related='company_id.foreign_currency_id')
     amount_total_signed_foreign = fields.Monetary(string='Total Signed', readonly=True, currency_field='company_foreign_currency_id')
     amount_residual_foreign = fields.Monetary(string='Amount Due', currency_field='company_foreign_currency_id')
+    amount_untaxed_signed_foreign = fields.Monetary(string='Untaxed Amount Signed', readonly=True, currency_field='company_foreign_currency_id')
 
-    @api.depends('amount_total_signed', 'amount_residual')
+    @api.depends('amount_total_signed', 'amount_residual', 'amount_untaxed_signed')
     def _compute_foreigns(self):
         for rec in self:
             if rec.move_type in ['out_invoice','out_refund','in_invoice','in_refund'] and not rec.debit_origin_id:
@@ -21,10 +22,13 @@ class AccountMove(models.Model):
                 if rec.currency_id == rec.company_foreign_currency_id:
                     rec.amount_total_signed_foreign = rec.amount_total_in_currency_signed
                     rec.amount_residual_foreign = rec.amount_residual
+                    rec.amount_untaxed_signed_foreign = rec.amount_untaxed_signed
                 else:
                     rec.amount_total_signed_foreign = rec.currency_id._convert(rec.amount_total_in_currency_signed, rec.company_foreign_currency_id, rec.company_id, rec.date)
                     rec.amount_residual_foreign = rec.currency_id._convert(rec.amount_residual, rec.company_foreign_currency_id, rec.company_id, rec.date)
+                    rec.amount_untaxed_signed_foreign = rec.currency_id._convert(rec.amount_untaxed_signed, rec.company_foreign_currency_id, rec.company_id, rec.date)
             else:
                 _logger.warning('nota de debito')
                 rec.amount_total_signed_foreign = 0
                 rec.amount_residual_foreign = 0
+                rec.amount_untaxed_signed_foreign = 0

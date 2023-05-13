f8e5d212773e651b2f24e0e4b00566b6c71660fa
Denidson Alvarez <dalvarez@code-war.com>
2023-05-12 20:34:06 -0400

    report foreign currency
    Add amount_untaxed_signed_foreign en Analisis de facturas

--- l10n_latam_foreing_currency/models/account_invoice_report.py
+++ l10n_latam_foreing_currency/models/account_invoice_report.py
@@ -16,7 +16,8 @@ class AccountInvoiceReport(models.Model):
         related='company_id.foreign_currency_id')
     amount_total_signed_foreign = fields.Monetary(string='Importe Moneda Extranjera', currency_field='company_foreign_currency_id')
     amount_residual_foreign = fields.Monetary(string='Importe Adeudado Moneda Extranjera', currency_field='company_foreign_currency_id')
+    amount_untaxed_signed_foreign = fields.Monetary(string='Impuestos no incluidos en Moneda Extranjera', currency_field='company_foreign_currency_id')
 
     def _select(self):
         return super(AccountInvoiceReport, self)._select() + ", move.amount_total_signed_foreign  \
-            as amount_total_signed_foreign, move.amount_residual_foreign as amount_residual_foreign"
+            as amount_total_signed_foreign, move.amount_residual_foreign as amount_residual_foreign, move.amount_untaxed_signed_foreign as amount_untaxed_signed_foreign"

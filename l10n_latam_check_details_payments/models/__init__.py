880de8d98a70d71a501c93f0c0de8f2c6aa46e67
Elvis Paez <elvispaez18@gmail.com>
2023-05-17 21:12:50 -0400

    [ADD] 16779

--- l10n_latam_check_details_payments/models/__init__.py
+++ l10n_latam_check_details_payments/models/__init__.py
@@ -7,3 +7,4 @@
 #
 ###############################################################################
 from . import account_payment
+from . import account_payment_group
---
+++ l10n_latam_check_details_payments/models/account_payment_group.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2023-Present.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+
+from odoo import models, fields, api, _, Command
+from odoo.exceptions import ValidationError, UserError
+
+
+class accountPaymentGroup(models.Model):
+    _inherit = 'account.payment.group'
+
+    def post(self):
+        for rec in self:
+            checks = rec.payment_ids.filtered(lambda x: x.payment_method_description == 'Cheque de Terceros Existente')
+            for element in checks:
+                if len(checks.filtered(lambda x: x.number_check_aux == element.number_check_aux)) > 1:
+                    raise UserError(_('No puede ingresar el mismo cheque mas de una vez'))
+        return super(accountPaymentGroup,self).post()

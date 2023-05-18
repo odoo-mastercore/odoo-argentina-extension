473c6ec1b299b23596cb3de455079e0baf1c90dc
Elvis Paez <elvispaez18@gmail.com>
2023-05-17 21:29:47 -0400

    [ADD] 16799

--- l10n_latam_check_details_payments/models/account_payment_group.py
+++ l10n_latam_check_details_payments/models/account_payment_group.py
@@ -18,6 +18,6 @@ class accountPaymentGroup(models.Model):
         for rec in self:
             checks = rec.payment_ids.filtered(lambda x: x.payment_method_description == 'Cheque de Terceros Existente')
             for element in checks:
-                if len(checks.filtered(lambda x: x.number_check_aux == element.number_check_aux)) > 1:
+                if len(checks.filtered(lambda x: x.number_check_aux == element.number_check_aux and x.l10n_latam_check_bank_id_aux == element.l10n_latam_check_bank_id_aux)) > 1:
                     raise UserError(_('No puede ingresar el mismo cheque mas de una vez'))
         return super(accountPaymentGroup,self).post()

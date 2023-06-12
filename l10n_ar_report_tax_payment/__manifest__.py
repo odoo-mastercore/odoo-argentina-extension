3dc1c31def3217dee5c8f4220c2d65155ef4574c
Elvis Paez <elvispaez18@gmail.com>
2023-06-12 13:57:53 -0400

    [ADD] 016920

---
+++ l10n_ar_report_tax_payment/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2023-Present.
+# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+{
+    "name": "Report tax payment",
+    "summary": "",
+    'version': "15.0.1",
+    'author': 'SINAPSYS GLOBAL SA || MASTERCORE SAS',
+    'website': "http://sinapsys.global",
+    'category': "sale",
+    "license": "AGPL-3",
+    "depends": ["account", "account_accountant", "account_payment_group"],
+    "data": [
+        'views/account_payment.xml'
+    ],
+}
---
+++ l10n_ar_report_tax_payment/models/account_payment.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2023-Present.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+
+from odoo import models, fields, api, _, Command
+from odoo.exceptions import ValidationError, UserError
+import logging
+import requests
+import json
+
+_logger = logging.getLogger(__name__)
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'

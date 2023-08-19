41b6e88d53d9e6df55762f05c8a349182cb86e70
Denidson Alvarez <dalvarez@code-war.com>
2023-08-18 22:11:14 -0400

    ADD #2308-018006 partner_report_currency

---
+++ partner_report_currency/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2021-Present.
+# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+{
+    'name': "Partner Report by Currency",
+    'version': "16.0.1",
+    'description': '',
+    'summary': "",
+    'author': "SINAPSYS GLOBAL SA || MASTERCORE SAS",
+    'website': "http://sinapsys.global",
+    'license': 'AGPL-3',
+    'category': 'Accounting',
+    'depends': ['base', 'account', 'account_accountant', 'account_reports'],
+    'installable': True,
+    'auto_install': False,
+    'assets': {
+        #'web.assets_backend': ['gl_foreign_currency/static/src/js/account_reports.js'],
+        },
+    'data': [
+        'view/views_actions.xml',
+        'view/partner_pending_payments.xml',
+    ],
+    'application': False,
+}

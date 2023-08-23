36c49c74638224a2f693e401f1bdcf7491706f8f
Elvis Paez <elvispaez18@gmail.com>
2023-08-23 17:46:57 -0400

    [REF] payments checks

--- l10n_latam_check_details_payments/models/account_payment.py
+++ l10n_latam_check_details_payments/models/account_payment.py
@@ -34,11 +34,14 @@ class accountPayment(models.Model):
     def create(self, vals_list):
         records = super().create(vals_list)
         for index, rec in enumerate(records):
-            print(vals_list)
             if vals_list[index].get('l10n_latam_check_number', False):
                 rec.number_check_aux = vals_list[index].get('l10n_latam_check_number')
             if vals_list[index].get('l10n_latam_check_bank_id', False):
                 rec.l10n_latam_check_bank_id_aux = vals_list[index].get('l10n_latam_check_bank_id')
+            if vals_list[index].get('check_number', False) == False and vals_list[index].get('l10n_latam_check_number', False):
+                rec.check_number = vals_list[index].get('l10n_latam_check_number')
+            if vals_list[index].get('l10n_latam_check_bank_id', False) == False and vals_list[index].get('l10n_latam_check_bank_id_aux', False):
+                rec.l10n_latam_check_bank_id = vals_list[index].get('l10n_latam_check_bank_id_aux')
         return records
     
     def write(self, vals):
@@ -49,5 +52,9 @@ class accountPayment(models.Model):
         if vals.get('l10n_latam_check_bank_id', False):
             vals.update({
                 'l10n_latam_check_bank_id_aux': vals.get('l10n_latam_check_bank_id', False)
+            })
+        if not vals.get('l10n_latam_check_bank_id', False) and self.l10n_latam_check_bank_id_aux:
+            vals.update({
+                'l10n_latam_check_bank_id': self.l10n_latam_check_bank_id_aux            
             })
         return super(accountPayment, self).write(vals)

d9077c36650aca3fe6eea99ae7f0223796f58ab8
Elvis Paez <elvispaez18@gmail.com>
2023-08-30 12:35:11 -0400

    [REF] same checks validation

--- l10n_latam_check_details_payments/models/account_payment_group.py
+++ l10n_latam_check_details_payments/models/account_payment_group.py
@@ -16,7 +16,9 @@ class accountPaymentGroup(models.Model):
 
     def post(self):
         for rec in self:
-            checks = rec.payment_ids.filtered(lambda x: x.payment_method_description == 'Cheque de Terceros Existente')
+            check_in_id = self.env.ref('l10n_latam_check.account_payment_method_in_third_party_checks').id
+            check_out_id = self.env.ref('l10n_latam_check.account_payment_method_out_third_party_checks').id
+            checks = rec.payment_ids.filtered(lambda x: x.payment_method_line_id.id in [check_in_id,check_out_id])
             for element in checks:
                 if len(checks.filtered(lambda x: x.number_check_aux == element.number_check_aux and x.l10n_latam_check_bank_id_aux == element.l10n_latam_check_bank_id_aux)) > 1:
                     raise UserError(_('No puede ingresar el mismo cheque mas de una vez'))

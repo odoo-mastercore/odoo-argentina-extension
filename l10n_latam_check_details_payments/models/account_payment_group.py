435c017069927ac68e0ac96b47ad6a951f3c89e5
Elvis Paez <elvispaez18@gmail.com>
2023-09-01 17:35:45 -0400

    [REF] latam check

--- l10n_latam_check_details_payments/models/account_payment_group.py
+++ l10n_latam_check_details_payments/models/account_payment_group.py
@@ -19,7 +19,7 @@ class accountPaymentGroup(models.Model):
             check_in_id = self.env.ref('l10n_latam_check.account_payment_method_in_third_party_checks').id
             check_out_id = self.env.ref('l10n_latam_check.account_payment_method_out_third_party_checks').id
             new_check = self.env.ref('l10n_latam_check.account_payment_method_new_third_party_checks').id
-            checks = rec.payment_ids.filtered(lambda x: x.payment_method_line_id.id in [check_in_id,check_out_id, new_check])
+            checks = rec.payment_ids.filtered(lambda x: x.payment_method_line_id.id in [check_in_id,check_out_id, new_check, 71])
             for element in checks:
                 if len(checks.filtered(lambda x: x.number_check_aux == element.number_check_aux and x.l10n_latam_check_bank_id_aux == element.l10n_latam_check_bank_id_aux)) > 1:
                     raise UserError(_('No puede ingresar el mismo cheque mas de una vez'))

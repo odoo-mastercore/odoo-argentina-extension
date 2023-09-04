e91ebafcf8f3c88ab23cdf495453ee1855f852ee
Elvis Paez <elvispaez18@gmail.com>
2023-09-03 21:47:44 -0400

    [ADD] 018051

---
+++ stock_remito_info_extended/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2023-Present.
+# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+{
+    "name": "Stock remito information extended",
+    "summary": "Agrega informacion adicional en el reporte del remito",
+    'version': "16.0.1",
+    'author': 'SINAPSYS GLOBAL SA || MASTERCORE SAS',
+    'website': "http://sinapsys.global",
+    'category': "stock",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/report_stock_picking.xml"
+    ],
+}

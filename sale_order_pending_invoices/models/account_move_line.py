b424130c7a9e85f2a5d9c8d4043f7e6b6f10ecd8
Monica Mujica <135365950+mmu-mastercore@users.noreply.github.com>
2023-11-09 12:39:35 -0400

    [UPD] sale_order_pending_invoices

--- sale_order_pending_invoices/models/account_move_line.py
+++ sale_order_pending_invoices/models/account_move_line.py
@@ -20,37 +20,53 @@ class AccountMoveLine(models.Model):
         string="Peding Invoices",
         compute='_get_pending_invoiced',
         copy=False)
+    open_invoice_count = fields.Integer(string="Invoice Count", compute='_get_pending_invoiced')
+    open_invoice_ids = fields.Many2many(
+        comodel_name='account.move',
+        string="Open Invoices",
+        compute='_get_pending_invoiced',
+        copy=False)
     
     @api.depends('order_line.invoice_lines')
     def _get_pending_invoiced(self):
-        # The invoice_ids are obtained thanks to the invoice lines of the SO
-        # lines, and we also search for possible refunds created directly from
-        # existing invoices. This is necessary since such a refund is not
-        # directly linked to the SO.
         for order in self:
             invoices = order.env['account.move'].search([('partner_id','=',order.partner_id.id)])
-            invoices = invoices.filtered(lambda r: r.move_type in ('out_invoice', 'out_refund') and r.payment_state in ('not_paid','partial'))
-            _logger.warning('pending_invoices %s', invoices)
-            order.pending_invoice_ids = invoices
-            order.pending_invoice_count = len(invoices)
+            open_invoices = invoices.filtered(lambda r: r.move_type in 
+                                        ('out_invoice', 'out_refund') and r.payment_state in ('not_paid','partial'))
+            _logger.warning('open_invoices %s', open_invoices)
+            pending_invoices = invoices.filtered(lambda r: r.move_type in 
+                                        ('out_invoice', 'out_refund') and 
+                                        r.payment_state in ('not_paid','partial') and fields.Date.today() >= r.invoice_date_due)
+            _logger.warning('pending_invoices %s', pending_invoices)
+            order.pending_invoice_ids = pending_invoices
+            order.pending_invoice_count = len(pending_invoices)
+            order.open_invoice_ids = open_invoices
+            order.open_invoice_count = len(open_invoices)
 
     def action_view_pending_invoice(self):
-        invoices = self.mapped('pending_invoice_ids')
+        pending_invoice = self.mapped('pending_invoice_ids')
         action = self.env['ir.actions.actions']._for_xml_id('sale_order_pending_invoices.action_pending_invoice')
-        if len(invoices) >= 1:
-            action['domain'] = [('id', 'in', invoices.ids)]
+        if len(pending_invoice) >= 1:
+            action['domain'] = [('id', 'in', pending_invoice.ids)]
         else:
             action = {'type': 'ir.actions.act_window_close'}
 
         context = {
             'default_move_type': 'out_invoice',
         }
-        if len(self) == 1:
-            context.update({
-                'default_partner_id': self.partner_id.id,
-                'default_partner_shipping_id': self.partner_shipping_id.id,
-                'default_invoice_payment_term_id': self.payment_term_id.id or self.partner_id.property_payment_term_id.id or self.env['account.move'].default_get(['invoice_payment_term_id']).get('invoice_payment_term_id'),
-                'default_invoice_origin': self.name,
-            })
+        action['context'] = context
+        return action      
+
+    def action_view_open_invoice(self):
+        open_invoice = self.mapped('open_invoice_ids')
+        action = self.env['ir.actions.actions']._for_xml_id('sale_order_pending_invoices.action_open_invoice')
+        if len(open_invoice) >= 1:
+            action['domain'] = [('id', 'in', open_invoice.ids)]
+        else:
+            action = {'type': 'ir.actions.act_window_close'}
+
+        context = {
+            'default_move_type': 'out_invoice',
+        }
         action['context'] = context
         return action      

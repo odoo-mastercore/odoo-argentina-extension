eda593deb60deeb823e6bdc62a83cf092780990d
Mastercore Sinapsys Global® <33432708+odoo-mastercore@users.noreply.github.com>
2024-06-10 19:12:55 +0000

    [UPD] l10n_latam_check_type

--- l10n_latam_check_type/__manifest__.py
+++ l10n_latam_check_type/__manifest__.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
-# Copyleft: 2023-Present.
-# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
-#
+# Author: Mastercore Sinapsys Global®
+# Copyright: 2019-Present.
+# License OPL-1 (Odoo Proprietary License v1.0) 
+# See https://www.odoo.com/documentation/master/legal/licenses.html
 #
 ###############################################################################
 {
-    "name": "l10n latam check type",
-    "summary": "",
-    'version': "16.0.1",
-    'author': 'SINAPSYS GLOBAL SA || MASTERCORE SAS',
-    'website': "http://sinapsys.global",
+    "name": "Mastercore L10N Custom - L10n Latam Check type",
+    'summary': """
+        The l10n_latam_check_type module enhances the Odoo localization 
+        for Argentina by introducing support for different check types 
+        specific to the region. It allows businesses to manage and process 
+        various types of checks, ensuring compliance with local financial regulations. 
+    """
+    'version': "16.0.1.1",
+    'author': "Mastercore Sinapsys Global®",
+    'website': "https://www.mastercore.us",
+    'sequence': 1,
     'category': "sale",
     "license": "AGPL-3",
     "depends": [
@@ -20,4 +26,6 @@
     "data": [
         'views/account_payment_view.xml'
     ],
+    'installable': True,
+    'auto_install': False
 }

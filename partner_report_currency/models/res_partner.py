8748a39eb0a3fc08e3ac9b46d79bb860b03ed6d1
Elvis Páez <elvispaez18@gmail.com>
2024-08-28 18:07:29 -0400

    [FIX] partner report currency

--- partner_report_currency/models/res_partner.py
+++ partner_report_currency/models/res_partner.py
@@ -36,7 +36,8 @@ class ResPartner(models.Model):
             company.append({
                 'name': comp.name,
                 'company_registry': comp.company_registry,
-                'vat': (comp.l10n_latam_identification_type_id.l10n_ve_code if ('l10n_latam_identification_type_id' in comp and len(comp.l10n_latam_identification_type_id) > 0) else '') + comp.vat,
+                # 'vat': (comp.l10n_latam_identification_type_id.l10n_ve_code if ('l10n_latam_identification_type_id' in comp and len(comp.l10n_latam_identification_type_id) > 0) else '') + comp.vat,
+                'vat': comp.vat,
                 'logo': comp.logo,
                 'phone': comp.phone,
                 'mobile': comp.mobile,

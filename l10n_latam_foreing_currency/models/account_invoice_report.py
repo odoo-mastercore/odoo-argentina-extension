028f759a10ad655979b328a20fb81e90e3bdd6f9
Mastercore Sinapsys Global® <33432708+odoo-mastercore@users.noreply.github.com>
2024-09-24 19:30:31 -0300

    [RES] l10n_latam_foreing_currency

---
+++ l10n_latam_foreing_currency/models/account_invoice_report.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2021-Present.
+# License LGPL-3.0 or later (http: //www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+from odoo import models, fields, api, _
+
+class AccountInvoiceReport(models.Model):
+
+    _inherit = 'account.invoice.report'
+
+    company_foreign_currency_id = fields.Many2one(
+        string='Foreign Company Currency', readonly=True,
+        related='company_id.foreign_currency_id'
+    )
+    price_subtotal_foreign = fields.Monetary('Subtotal en Divisa',
+        currency_field='company_foreign_currency_id'
+    )
+
+    def _select(self):
+        return super(AccountInvoiceReport, self)._select() + """,
+        line.price_subtotal_foreign as price_subtotal_foreign
+        """

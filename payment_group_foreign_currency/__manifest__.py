ecbc92812abda411a860c0a064bdc306ce535772
rmi-mastercore <rmi@mastercore.us>
2024-09-27 21:37:19 -0400

    [IMP]Add a number to the version :3 #21858

--- payment_group_foreign_currency/__manifest__.py
+++ payment_group_foreign_currency/__manifest__.py
@@ -9,7 +9,7 @@
 {
     "name": "Payment Group in Foreign currency",
     "summary": "",
-    'version': "16.0.2.2",
+    'version': "16.0.2.3",
     'author': 'SINAPSYS GLOBAL SA || MASTERCORE SAS',
     'website': "http://sinapsys.global",
     'category': "contact",

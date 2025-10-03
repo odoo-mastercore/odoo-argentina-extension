d81bb0f33f7717f83b7246db3f2ccf421e3c9a48
José Bracho <brachojgbp@gmail.com>
2025-10-03 19:34:22 -0400

    [IMP] #odoo/all-tickets/26321

--- l10n_ar_tax_extended/models/account_payment.py
+++ l10n_ar_tax_extended/models/account_payment.py
@@ -6,7 +6,7 @@
 # See https://www.odoo.com/documentation/master/legal/licenses.html
 #
 ################################################################################
-from odoo import api, models, fields, _
+from odoo import api, models, fields, _, Command
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -32,3 +32,28 @@ class AccountPayment(models.Model):
                             'amount_currency': line.amount_currency
                         })
         return res
+
+    @api.depends("l10n_ar_fiscal_position_id", "partner_id", "company_id", "date")
+    def _compute_l10n_ar_withholding_line_ids(self):
+        """
+            Este metodo unifica la logica entre l10n_ar_withholding y l10n_ar_tax_extended
+            manteniendo prioridad a la configuracion de la posicion fiscal.
+        """
+        for rec in self.filtered(lambda x: x.partner_type == "supplier"):
+            date = rec.date or fields.Date.context_today(self)
+            withholdings = [Command.clear()]
+            if rec.l10n_ar_fiscal_position_id.l10n_ar_tax_ids:
+                taxes = rec.l10n_ar_fiscal_position_id._l10n_ar_add_taxes(
+                    rec.partner_id, rec.company_id, date, "withholding"
+                )
+                withholdings += [Command.create({"tax_id": x.id}) for x in taxes]
+            else:
+                partner_taxes = self.env['l10n_ar.partner.tax'].search([
+                    *self.env['l10n_ar.partner.tax']._check_company_domain(rec.company_id),
+                    '|', ('from_date', '>=', date), ('from_date', '=', False),
+                    '|', ('to_date', '<=', date), ('to_date', '=', False),
+                    ('partner_id', '=', rec.partner_id.commercial_partner_id.id),
+                    ('tax_id.l10n_ar_withholding_payment_type', '=', rec.partner_type)
+                ])
+                withholdings = [Command.clear()] + [Command.create({'tax_id': x.tax_id.id}) for x in partner_taxes]
+            rec.l10n_ar_withholding_line_ids = withholdings

562bc9cc65d4945ea0ee3f602c12b6405b181256
José Bracho <brachojgbp@gmail.com>
2025-12-15 10:30:06 -0400

    [FIX] bugfix l10n_ar_currency_rate_button

--- l10n_ar_currency_rate_button/__manifest__.py
+++ l10n_ar_currency_rate_button/__manifest__.py
@@ -14,7 +14,7 @@
     """,
     'author': 'SINAPSYS GLOBAL SA || MASTERCORE SAS',
     'website': 'www.sinapsys.global',
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.0.2",
     'license': 'AGPL-3',
     'category': 'account',
     'depends': [],

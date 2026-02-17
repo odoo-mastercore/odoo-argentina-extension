17ec8374d22cc1922a137c335700551fc63d5486
odoo-mastercore <github@sinapsys.global>
2026-02-17 00:58:08 -0300

    [IMP] l10n_ar_hr_payroll: set payroll data names in Spanish

--- l10n_ar_hr_payroll/__manifest__.py
+++ l10n_ar_hr_payroll/__manifest__.py
@@ -13,7 +13,7 @@
     "category": "Human Resources/Payroll",
     "depends": ["l10n_ar", "hr_payroll", "hr_work_entry_holidays", "hr_payroll_holidays"],
     "auto_install": ["hr_payroll"],
-    "version": "1.0.3",
+    "version": "1.0.4",
     "description": """
 Argentinian Payroll Rules.
 ==========================
